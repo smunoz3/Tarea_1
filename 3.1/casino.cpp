905c108e6cb0b871c3121ed6400c043fe0233617
smunoz3 <sebamunoz003@gmail.com>
2023-04-04 16:04:06 -0400

    base 3.1

--- 3.1/casino.cpp
+++ 3.1/casino.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+int const SERV_DESAYUNO = 0;
+int const SERV_ALMUERZO = 1;
+int const SERV_ONCE = 2;
+int const SERV_CENA = 3;
+
+struct SaldoColaborado{
+    char rut [11];
+    int saldo_desayuno;
+    int saldo_almuerzo;
+    int saldo_once;
+    int saldo_cena; 
+};
+
+bool puedeConsumir(char* rut, int servicio, string consumos_dia){}
+;
+
 int main(){
         
     return 0;
