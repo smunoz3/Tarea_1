51f6cfb1e9f4939f39e16594048e1f164c5b3a4b
Atendiendo <huertamiguel527@gmail.com>
2023-04-08 14:53:32 -0400

    Creacion funcion comprarTarjeta en listas_suerte

--- 3.2/listas_suerte.cpp
+++ 3.2/listas_suerte.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Persona {
+    string nombre;
+    char fecha[11];
+    int tamanio_tarjeta;
+    int* tarjeta;
+    bool quiere_intercambiar;
+};
+
+int* comprarTarjeta (string nombre, int dia, int& m){
+    m = nombre.length();
+
+    int* tarjeta= new int[m];
+
+    for (int i=0; i<m; i++){
+        tarjeta[i]= nombre[i]%dia;
+    }
+    return tarjeta;
+
+}
 
 int main(){
-        
+    //PARA PROBAR LA FUNCION comprarTarjeta y visualizar lo que contiene cada string de enteros
+    //*****************
+    int m;
+    int* tarjeta = comprarTarjeta("Maria",2,m);
+    for ( int i=0 ; i < m ; i++){
+        cout << tarjeta[i] << endl;
+    }
+    delete tarjeta;
+    //**********************
     return 0;
 }
