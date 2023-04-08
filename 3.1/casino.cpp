8dceb87bb71f6fe1fd9bc0668c95667715952e1f
smunoz3 <sebamunoz003@gmail.com>
2023-04-07 20:04:42 -0400

    casino_usm

--- 3.1/casino.cpp
+++ 3.1/casino.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -14,55 +15,88 @@ struct SaldoColaborado{  // estructura binario
     int saldo_desayuno;
     int saldo_almuerzo;
     int saldo_once;
-    int saldo_cena; 
+    int saldo_cena;
 };
 
 bool puedeConsumir(char* rut, int servicio, string consumos_dia){
-    bool n;
-    n = true;
-    //string servicio_salida ="";
-    ofstream file;
+    bool retorno;
+    retorno = false;
+    string rut_string = rut;
+
+    fstream file_binario;        //abrir archivo binario
+    file_binario.open("saldos.bin",ios::binary| ios::in);
+    if (!file_binario.is_open()){
+        cout<<"Error \n";exit(1);
+        }
+    ofstream file;          //abrir archivo txt
     file.open(consumos_dia+".txt",ios::app);
     if (!file.is_open()){
         cout<<"Error \n";exit(1);
-        };
+        }
+
     string comidas_array [4];
     comidas_array[0]="DESAYUNO";comidas_array[1]="ALMUERZO";comidas_array[2]="ONCE";comidas_array[3]="CENA";
-    /* if (SERV_DESAYUNO  ==  servicio){
-        servicio_salida = "DESAYUNO";
-    }
-    else if   (SERV_ALMUERZO  ==  servicio){
-        servicio_salida = "ALMUERZO";
-    }
-    else if  (SERV_ONCE  ==  servicio){
-        servicio_salida = "ONCE";
-    }
-    else if (SERV_CENA  ==  servicio){
-        servicio_salida = "CENA";
-    }
-    else {
-        servicio_salida = "Error PAPU :V\n"; 
-    }*/
-    file<<rut<<" "<<comidas_array[servicio] <<"\n";
+    
+    int saldo_size;
+    file_binario.read((char*)&saldo_size,sizeof(int));
 
+    SaldoColaborado saldo_lectura [saldo_size];
+    for (int i=0;i<saldo_size;i++){
+        file_binario.read((char*)&saldo_lectura[i],sizeof(SaldoColaborado));
+        if (strcmp(rut , saldo_lectura[i].rut)== 0){
+            if (servicio == SERV_DESAYUNO ){
+                if (saldo_lectura[i].saldo_desayuno>0){
+                    retorno = true;
+                    file <<rut << " " + comidas_array[servicio] + "\n";
+                }
+                else{
+                    cout<<"no desayuno"<<endl;
+                }
+            }
+            else if (servicio == SERV_ALMUERZO ){
+                if (saldo_lectura[i].saldo_almuerzo>0){
+                    retorno = true;
+                    file <<rut << " " + comidas_array[servicio] + "\n";
+                }
+                else{
+                    cout<<"no almuerzo"<<endl;
+                }
+            }
+            else if (servicio == SERV_ONCE ){
+                if (saldo_lectura[i].saldo_once>0){
+                    retorno = true;
+                    file <<rut << " " + comidas_array[servicio] + "\n";
+                }
+                else{
+                    cout<<"no once"<<endl;
+                }
+            }
+            else if (servicio == SERV_CENA ){
+                if (saldo_lectura[i].saldo_cena>0){
+                    retorno = true;
+                    file <<rut << " " + comidas_array[servicio] + "\n";
+                }
+                else{
+                    cout<<"no cena"<<endl;
+                }
+            }
+            
+        }
+        else{
+            cout<<"no rut"<<endl;
+        }
 
+    }
+    cout<<retorno<<endl;
     file.close();
-
-
-
-    return n;
+    file_binario.close();
+    return retorno;
 };
 
-/*
-fstream fp;
-fp.open("saldos.bin",ios::binary|ios:out);
-fp.write("21270184-K", 1, 1 ,0, 0);
-*/
-
 int main(){
-        bool test = puedeConsumir("21270184-K",0,"Consumos");
-        bool test2 = puedeConsumir("21298184-1",2,"Consumos");
-        bool test3= puedeConsumir("21298184-1",7,"Consumos");
-        cout<<test<<test2<<test3<<endl;
+        bool test = puedeConsumir("11111111-1",0,"Consumos");
+        bool test2 = puedeConsumir("11111111-1",2,"Consumos");
+        bool test3= puedeConsumir("11111111-1",7,"Consumos");
+        bool test4 = puedeConsumir("22222222-2",2,"Consumos");
     return 0;
 }
