b799d7d0ff725c36acfc22214daea9ebf8fcf981
Atendiendo <huertamiguel527@gmail.com>
2023-04-15 19:02:51 -0400

    Creada funcion variosDias

--- 3.2/listas_suerte.cpp
+++ 3.2/listas_suerte.cpp
@@ -11,8 +11,9 @@ struct Persona {
     bool quiere_intercambiar;
 };
 
+int p;
+
 int* comprarTarjeta (string nombre, int dia, int& m){
-    m = nombre.length();
 
     int* tarjeta= new int[m];
 
@@ -26,15 +27,15 @@ int* comprarTarjeta (string nombre, int dia, int& m){
 //Hacer un array de structs de las personas
 
 //ESTA FUNCION LA VAN A CREAR LOS AYUDANTES
-Persona* arregloPersonas(int& tamanio_array){
+Persona* arregloPersonas(int& p){
     int m;
-    cin >> tamanio_array;
-    Persona* personas = new Persona[tamanio_array];
-    for (int i=0; i<tamanio_array; i++){
+    p;
+    Persona* personas = new Persona[p];
+    for (int i=0; i<p; i++){
         cin >> personas[i].nombre;
         cin >> personas[i].fecha;
         personas[i].tamanio_tarjeta = personas[i].nombre.length();
-        personas[i].tarjeta = comprarTarjeta(personas[i].nombre,2,m); //ACA SE PUEDE CAMBIAR EL DIA IMPORTANTE
+        //personas[i].tarjeta = comprarTarjeta(personas[i].nombre,2,m); //ACA SE PUEDE CAMBIAR EL DIA IMPORTANTE
         cin >> personas[i].quiere_intercambiar;
     }
     return personas; 
@@ -78,7 +79,51 @@ int puntaje(Persona* p1){
 }
 //NO ME SIRVE ESTA WEA ME QUIERO MATAR
 
+Persona* unDia(Persona* personas, int dia){
+    int n_intercambios = 0;
+    for(int i=0; i<p; i++){
+        int largo_nombre;
+        largo_nombre = personas[i].nombre.length();
+        personas[i].tarjeta = comprarTarjeta(personas[i].nombre, dia, largo_nombre); //Aca se le da una tarjeta a cada persona
+        if (personas[i].quiere_intercambiar==1){
+            n_intercambios++;
+        }
+    }//HASTA ACA FUNCIONA
+    int contador=0;
+    Persona* personaintercambio = new Persona [contador];
+    for (int i=0; i<p; i++){
+        if (personas[i].quiere_intercambiar==1){
+            personaintercambio[contador]=personas[i];//Arreglo de personas que quieren intercambiar
+            contador++;
+        }
+    }
+    
+    for(int i=1; i<=n_intercambios; i+=2){
+        int largo_tarjeta_menor = (personaintercambio[i-1].tamanio_tarjeta<=personaintercambio[i].tamanio_tarjeta) ? personaintercambio[i-1].tamanio_tarjeta : personaintercambio[i].tamanio_tarjeta;
+        for(int j=0; j<largo_tarjeta_menor; j++){
+            if (personaintercambio[i-1].tarjeta[j] == personaintercambio[i].tarjeta[j]){
+                Persona* p1 = &personaintercambio[i-1];
+                Persona* p2 = &personaintercambio[i];
+                intercambiarTarjeta(p1,p2);// ACA SE CAMBIAN LAS TARJETAS EN EL NUEVO ARREGLO, REVISAR SI CAMBIA AL STRUCT ORIGINAL
+                break;
+            }
+        }
+    }
+    delete [] personaintercambio;
+    
+}
+
+void variosDias (Persona* personas, int cant_dias){ //TOO EZ
+    Persona* ganador;
+    for (int i=1; i<=cant_dias; i++){
+        ganador = unDia(personas,i);
+        cout << ganador->nombre << " " << ganador->fecha << " " << puntaje(ganador) << endl;
+    }
+    
+}
+
 int main(){
+    cin >> p;
     //PARA PROBAR LA FUNCION comprarTarjeta y visualizar lo que contiene cada array de enteros
     //*****************
     /*
@@ -94,9 +139,9 @@ int main(){
    //PARA PROBAR LA FUNCION arregloPersonas y visualizar lo que contiene cada struct
    //***********************
    /*
-    int tamanio_array;
-    Persona* personas = arregloPersonas(tamanio_array);
-    for (int i = 0; i<tamanio_array; i++){
+    int p;
+    Persona* personas = arregloPersonas(p);
+    for (int i = 0; i<p; i++){
         cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
         for ( int j=0; j < (int)personas[i].nombre.length(); j++){
             if(j==0){
@@ -119,9 +164,9 @@ int main(){
    //PARA PROBAR LA FUNCION intercambiarTarjeta
    //********************
    /*
-   int tamanio_array;
-    Persona* personas = arregloPersonas(tamanio_array);
-    for (int i = 0; i<tamanio_array; i++){
+   int p;
+    Persona* personas = arregloPersonas(p);
+    for (int i = 0; i<p; i++){
         cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
         for ( int j=0; j < (int)personas[i].nombre.length(); j++){
             if(j==0){
@@ -140,7 +185,7 @@ int main(){
     p1 = &personas[0];
     p2 = &personas[2];
     intercambiarTarjeta(p1,p2);
-    for (int i = 0; i<tamanio_array; i++){
+    for (int i = 0; i<p; i++){
         cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
         for ( int j=0; j < (int)personas[i].nombre.length(); j++){
             if(j==0){
@@ -159,10 +204,11 @@ int main(){
 
 
     //PARA PROBAR LA FUNCION puntaje
-
-    int tamanio_array;
-    Persona* personas = arregloPersonas(tamanio_array); //Crear los structs
-    for (int i = 0; i<tamanio_array; i++){
+    //***************************
+    /*
+    int p;
+    Persona* personas = arregloPersonas(p); //Crear los structs
+    for (int i = 0; i<p; i++){
         cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
         for ( int j=0; j < (int)personas[i].nombre.length(); j++){
             if(j==0){
@@ -181,7 +227,46 @@ int main(){
     int puntos;
     puntos = puntaje(p1);
     cout << "\n" << puntos;
-    for (int i=0; i<tamanio_array; i++){
+    for (int i=0; i<p; i++){
+        delete personas[i].tarjeta;
+    }
+    */
+   //******************************************
+
+
+   //PARA PROBAR LA FUNCION unDia
+   int p;
+    Persona* personas = arregloPersonas(p); //Crear los structs
+    for (int i = 0; i<p; i++){
+        cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
+        for ( int j=0; j < (int)personas[i].nombre.length(); j++){
+            if(j==0){
+                cout << "[";
+            }
+            cout << personas[i].tarjeta[j] << ",";
+            if(j==((int)personas[i].nombre.length())-1){
+                cout << "]";
+            }
+        }
+        cout << " " << personas[i].quiere_intercambiar << endl;
+    }
+
+    unDia(personas,2);
+
+    for (int i = 0; i<p; i++){
+        cout << personas[i].nombre << " " << personas[i].fecha << " " << personas[i].tamanio_tarjeta << " ";
+        for ( int j=0; j < (int)personas[i].nombre.length(); j++){
+            if(j==0){
+                cout << "[";
+            }
+            cout << personas[i].tarjeta[j] << ",";
+            if(j==((int)personas[i].nombre.length())-1){
+                cout << "]";
+            }
+        }
+        cout << " " << personas[i].quiere_intercambiar << endl;
+    }
+    for (int i=0; i<p; i++){
         delete personas[i].tarjeta;
     }
     return 0;
